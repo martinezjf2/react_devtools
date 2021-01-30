a48c258a32ef1546bda54dab2ad0cc7dddb48706
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-29 22:24:18 -0500

    created Avatar component and rearranged the imp to the avatar component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,6 @@ function Card(props) {
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
       </div>
       <div className="bottom">
         <p className="info">{props.tel}</p>
