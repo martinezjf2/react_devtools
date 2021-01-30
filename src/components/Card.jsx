f2347273d8d7b7e6b595eaac5168685152a7b285
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-29 22:42:14 -0500

    moved the p tags to the details component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,10 +9,7 @@ function Card(props) {
         <h2 className="name">{props.name}</h2>
         <Avatar img={props.img} />
       </div>
-      <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
-      </div>
+      <div className="bottom"></div>
     </div>
   );
 }
